a7e485a6c52ebd4dfd9bc2ea6365ae81fecae329
Lu Junjie <57125479+Lu-tju@users.noreply.github.com>
2020-06-07 17:06:08 +0800

    Add files via upload

---
+++ U_Net/main.py
@@ -0,0 +1,87 @@
+import torch
+import argparse
+from torch.utils.data import DataLoader
+from torch import nn, optim
+from torchvision.transforms import transforms
+from unet import Unet
+from dataset import LiverDataset
+
+# 是否使用cuda
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+x_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+])
+
+# mask只需要转换为tensor
+y_transforms = transforms.ToTensor()
+
+
+def train_model(model, criterion, optimizer, dataload, num_epochs=20):
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+        dt_size = len(dataload.dataset)
+        epoch_loss = 0
+        step = 0
+        for x, y in dataload:
+            step += 1
+            inputs = x.to(device)
+            labels = y.to(device)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # forward
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+            print("%d/%d,train_loss:%0.3f" % (step, (dt_size - 1) // dataload.batch_size + 1, loss.item()))
+        print("epoch %d loss:%0.3f" % (epoch, epoch_loss / step))
+    torch.save(model.state_dict(), 'weights_%d.pth' % epoch)
+    return model
+
+
+# 训练模型
+def train(args):
+    model = Unet(3, 1).to(device)
+    batch_size = args.batch_size
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters())
+    liver_dataset = LiverDataset("data/train", transform=x_transforms, target_transform=y_transforms)
+    dataloaders = DataLoader(liver_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    train_model(model, criterion, optimizer, dataloaders)
+
+
+# 测试模型
+def test(args):
+    model = Unet(3, 1).to(device)   # 构建模型
+    model.load_state_dict(torch.load(args.ckpt, map_location='cpu'))    # 加载参数
+    liver_dataset = LiverDataset("data/val", transform=x_transforms, target_transform=y_transforms)  # 加载数据，这里懒了用的train的函数，所以也要加载mask，不过识别时没用到
+    dataloaders = DataLoader(liver_dataset, batch_size=1)
+    model.eval()
+    import matplotlib.pyplot as plt
+    plt.ion()
+    with torch.no_grad():
+        for x, _ in dataloaders:
+            y = model(x.to(device))
+            img_y = torch.squeeze(y.cpu()).numpy() > 0
+            plt.imshow(img_y)
+            plt.pause(0.01)
+        plt.show()
+
+
+if __name__ == '__main__':
+    # 参数解析
+    parse = argparse.ArgumentParser()
+    parse = argparse.ArgumentParser()
+    parse.add_argument("--action", type=str, default='test', help="train or test")
+    parse.add_argument("--batch_size", type=int, default=4)
+    parse.add_argument("--ckpt", type=str, default='weights.pth', help="the path of model weight file")
+    args = parse.parse_args()
+
+    if args.action == "train":
+        train(args)
+    elif args.action == "test":
+        test(args)
